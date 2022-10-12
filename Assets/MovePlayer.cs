a9c8cc3b46908774023fdd9d9a42f4d7698c90f9
Sondre Schirmer-Mikalsen <sondremikalsen1@gmail.com>
2022-10-12 11:47:30 +0200

    moving player

---
+++ Assets/MovePlayer.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public float horizontalMovement;
+    public float verticalMovement;
+
+    // A field editable from inside Unity with a default value of 5
+    public float speed = 5.0f;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // This will detect forward and backward movement
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+
+        // This will detect sideways movement
+        verticalMovement = Input.GetAxisRaw("Vertical");
+
+        // Calculate the direction to move the player
+        Vector3 movementDirection = transform.forward * verticalMovement + transform.right * horizontalMovement;
+    
+        // Move the player
+        rb.AddForce(movementDirection * speed, ForceMode.Force);
+    }
+}
