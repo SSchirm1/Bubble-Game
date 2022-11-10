e8a4ac28a3f36fd9682285930f970a2f0258c042
Sondre Schirmer-Mikalsen <sondremikalsen1@gmail.com>
2022-11-10 13:40:41 +0100

    added prison scene

--- Assets/pakker/PowerUp/Scripts/TakeablePowerUp.cs
+++ Assets/pakker/PowerUp/Scripts/TakeablePowerUp.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class TakeablePowerUp : MonoBehaviour {
 	CustomizablePowerUp customPowerUp;
     private Rigidbody rb;
+    public float lives = 1;
     void Start() {
 		customPowerUp = (CustomizablePowerUp)transform.parent.gameObject.GetComponent<CustomizablePowerUp>();
 		//this.audio.clip = customPowerUp.pickUpSound;
@@ -13,8 +15,10 @@ public class TakeablePowerUp : MonoBehaviour {
 		if(collider.tag == "Player") {
             rb = collider.GetComponent<Rigidbody>();
 			rb.AddForce(0, 5000f, 0);
-           
-			PowerUpManager.Instance.Add(customPowerUp);
+            lives += 1;
+            Debug.Log("Lives: " + lives);
+
+            PowerUpManager.Instance.Add(customPowerUp);
 			if(customPowerUp.pickUpSound != null){
 				AudioSource.PlayClipAtPoint(customPowerUp.pickUpSound, transform.position);
 			}
