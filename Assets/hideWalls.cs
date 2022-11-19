f5f0c04166741e9cb58cd0be26593d3399031326
Sondre Schirmer-Mikalsen <sondremikalsen1@gmail.com>
2022-11-19 23:37:18 +0100

    obstacle

--- Assets/hideWalls.cs
+++ Assets/hideWalls.cs
@@ -7,6 +7,7 @@ public class hideWalls : MonoBehaviour
 
     public GameObject player;
     public LayerMask layer;
+    private Color originalColor;
     Renderer r;
     // Start is called before the first frame update
     void Start()
@@ -25,20 +26,30 @@ public class hideWalls : MonoBehaviour
         float cameraPlayerDistance = Vector3.Magnitude(transform.position - player.transform.position);
         RaycastHit hit;
         Physics.Raycast(transform.position, player.transform.position, out hit, cameraPlayerDistance, layer);
-
+        
         if (hit.collider != null)
         {
             Debug.Log(hit.collider.gameObject.name);
             r = hit.collider.GetComponent<Renderer>();
+            originalColor = r.sharedMaterial.color;
             if (r)
             {
-                StartCoroutine(waiter());
-               
-               
+                Debug.Log("Hit object" + hit.collider.gameObject.name);
+                // Change the material of all hit colliders
+                // to use a transparent shader.
+                r.material.shader = Shader.Find("Transparent/Diffuse");
+                 Color tempColor = r.material.color;
+                 tempColor.a = 0.3F;
+                 r.material.color = tempColor;
             }
+            
 
 
         }
+        else {
+                Debug.Log("Showing object");
+                hit.collider.gameObject.GetComponent<Material>().color = originalColor;
+            }
 
     }
 
