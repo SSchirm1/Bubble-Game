4817459c176e5af210ec1ed7804c38a638b46fa2
andrhae <andrhae@stud.ntnu.no>
2022-11-24 15:12:49 +0100

    portal script

--- Assets/Scripts/PortalScript.cs
+++ Assets/Scripts/PortalScript.cs
@@ -5,12 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class PortalScript : MonoBehaviour
 {
+    public Material portalMat;
 
     private void OnTriggerEnter(Collider collided) 
     {
         if (ScoreScript.allCollected) 
         {
             SceneManager.LoadScene(2); // win scene
+        }
+    }
+
+    private void Update()
+    {
+        if (ScoreScript.allCollected) {
+            gameObject.GetComponent<MeshRenderer>().material = portalMat;
         }
     }
     
